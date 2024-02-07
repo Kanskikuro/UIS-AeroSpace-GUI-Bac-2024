e12fd56c7d5b96a14543208a938b223c762a7269
Ninh Bao Truong <nb.truong@stud.uis.no>
2024-02-07 13:52:45 +0100

    add connectionstring and migration

--- Borealis2tsx/Borealis2tsx.Server/Program.cs
+++ Borealis2tsx/Borealis2tsx.Server/Program.cs
@@ -1,4 +1,7 @@
 using Borealis2tsx.Server.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore.Design;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +14,23 @@ builder.Services.AddScoped<IReadDataPortService, ReadDataPortService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+// DB connectionstring
+var connection = String.Empty;
+if (builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddEnvironmentVariables().AddJsonFile("appsettings.Development.json");
+    connection = builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
+}
+else
+{
+    connection = Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTIONSTRING");
+}
+
+builder.Services.AddDbContext<PersonDbContext>(options =>
+    options.UseSqlServer(connection));
+
 
 var app = builder.Build();
 
@@ -32,4 +52,37 @@ app.MapControllers();
 
 app.MapFallbackToFile("/index.html");
 
+
+// test person table in DB
+app.MapGet("/Person", (PersonDbContext context) =>
+{
+    return context.Person.ToList();
+})
+.WithName("GetPersons");
+
+app.MapPost("/Person", (Person person, PersonDbContext context) =>
+{
+    context.Add(person);
+    context.SaveChanges();
+})
+.WithName("CreatePerson");
+
 app.Run();
+
+
+public class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+public class PersonDbContext : DbContext
+{
+    public PersonDbContext(DbContextOptions<PersonDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Person> Person { get; set; }
+}
