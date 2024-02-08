78d9084b40d1ed3da27e7162510c2b2d5b4f2027
Ninh Bao Truong <nb.truong@stud.uis.no>
2024-02-08 13:17:56 +0100

    lagte person tabell i database og fungerer

--- Borealis2tsx/Borealis2tsx.Server/Program.cs
+++ Borealis2tsx/Borealis2tsx.Server/Program.cs
@@ -1,7 +1,6 @@
 using Borealis2tsx.Server.Services;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.SqlServer;
-using Microsoft.EntityFrameworkCore.Design;
+using Borealis2tsx.Server.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,19 +16,8 @@ builder.Services.AddSwaggerGen();
 
 
 // DB connectionstring
-var connection = String.Empty;
-if (builder.Environment.IsDevelopment())
-{
-    builder.Configuration.AddEnvironmentVariables().AddJsonFile("appsettings.Development.json");
-    connection = builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
-}
-else
-{
-    connection = Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTIONSTRING");
-}
-
 builder.Services.AddDbContext<PersonDbContext>(options =>
-    options.UseSqlServer(connection));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
 var app = builder.Build();
@@ -52,37 +40,4 @@ app.MapControllers();
 
 app.MapFallbackToFile("/index.html");
 
-
-// test person table in DB
-app.MapGet("/Person", (PersonDbContext context) =>
-{
-    return context.Person.ToList();
-})
-.WithName("GetPersons");
-
-app.MapPost("/Person", (Person person, PersonDbContext context) =>
-{
-    context.Add(person);
-    context.SaveChanges();
-})
-.WithName("CreatePerson");
-
 app.Run();
-
-
-public class Person
-{
-    public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-}
-
-public class PersonDbContext : DbContext
-{
-    public PersonDbContext(DbContextOptions<PersonDbContext> options)
-        : base(options)
-    {
-    }
-
-    public DbSet<Person> Person { get; set; }
-}
